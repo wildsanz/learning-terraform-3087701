d16a00b393e71fc75fedbcad881444e15b82e8b1
wildsanz <135728723+wildsanz@users.noreply.github.com>
2025-11-04 21:07:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t2.nano"
+  instance_type = "t3.small"
 
   tags = {
     Name = "HelloWorld"
